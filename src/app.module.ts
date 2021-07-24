ee1f7e4547a48bd18f4d81e74f10fa142ef58853
Luis Guilherme(Shadow) <sena.luisgf@gmail.com>
2021-07-24 17:56:13 -0400

    using environment variable to connect with mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,22 @@ import { Module } from '@nestjs/common';
 import { CatModule } from './cat/cat.module';
 import { DogModule } from './dog/dog.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { environment } from './environments';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 
 @Module({
   imports: [
     CatModule,
     DogModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://bsstore:BSStore@cluster0.aebtc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    ),
+    MongooseModule.forRoot(environment.database.mongo.uri),
+    // MongooseModule.forRootAsync({
+    //   imports:[ConfigModule],
+    //   useFactory: async (configService: ConfigService) =>({
+    //     uri: configService.get<string>('MONGO_URI')
+    //   }),
+    //   inject: [ConfigService]
+    // }),
   ],
   controllers: [],
   providers: [],
