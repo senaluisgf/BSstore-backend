a1d7837a72b3dad7dfe886576532127fb498b674
Luis Guilherme(Shadow) <sena.luisgf@gmail.com>
2021-07-24 18:13:15 -0400

    cats: creating a endpoint to get environment variables of different ways

--- src/cat/cat.service.ts
+++ src/cat/cat.service.ts
@@ -1,12 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ConfigService, ConfigType } from '@nestjs/config';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { environment } from 'src/environments';
+import databaseConfig from 'src/environments/databaseConfig';
 import { CreateCatDTO } from './dtos/create-cat.dto';
 import { CatModel } from './models/cat.model';
 
 @Injectable()
 export class CatService {
-  constructor(@InjectModel('CatModel') private catModel: Model<CatModel>) {}
+  constructor(
+    @InjectModel('CatModel') private catModel: Model<CatModel>,
+    private configService: ConfigService,
+    @Inject(databaseConfig.KEY)
+    private dbConfig: ConfigType<typeof databaseConfig>
+  ) {}
 
   async getCats() {
     return this.catModel.find();
@@ -19,5 +27,12 @@ export class CatService {
     } catch (erro) {
       throw new Error(erro.message);
     }
+  }
+
+  teste() {
+      const trabalho = environment.database.mongo.uri
+      const nest1 = this.configService.get<string>('database.mongo.uri')
+      const nest2 = this.dbConfig.mongo.uri
+      return {trabalho, nest1, nest2}
   }
 }
