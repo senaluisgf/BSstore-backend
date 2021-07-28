05b49603693cfe4e2b89436d27612c9e21c0ca47
Luis Guilherme(Shadow) <sena.luisgf@gmail.com>
2021-07-27 22:21:15 -0400

    removing nestjs configureModule scripts

--- src/cat/cat.service.ts
+++ src/cat/cat.service.ts
@@ -1,9 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { ConfigService, ConfigType } from '@nestjs/config';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { environment } from 'src/environments';
-import databaseConfig from 'src/environments/databaseConfig';
 import { CreateCatDTO } from './dtos/create-cat.dto';
 import { CatModel } from './models/cat.model';
 
@@ -11,9 +9,6 @@ import { CatModel } from './models/cat.model';
 export class CatService {
   constructor(
     @InjectModel('CatModel') private catModel: Model<CatModel>,
-    private configService: ConfigService,
-    @Inject(databaseConfig.KEY)
-    private dbConfig: ConfigType<typeof databaseConfig>
   ) {}
 
   async getCats() {
@@ -31,8 +26,6 @@ export class CatService {
 
   teste() {
       const trabalho = environment.database.mongo.uri
-      const nest1 = this.configService.get<string>('database.mongo.uri')
-      const nest2 = this.dbConfig.mongo.uri
-      return {trabalho, nest1, nest2}
+      return {trabalho}
   }
 }
